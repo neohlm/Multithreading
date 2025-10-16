1401852cdbe8e08d60a1b4e01ab16c8f37f8d8e0
neohlm <neohlumbene@gmail.com>
2025-10-16 09:48:45 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace multithreaded_code
+{
+    partial class Multithreading
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblHeading = new System.Windows.Forms.Label();
+            this.btnRun = new System.Windows.Forms.Button();
+            this.prbProcessProgress = new System.Windows.Forms.ProgressBar();
+            this.btnShowMessage = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblHeading
+            // 
+            this.lblHeading.AutoSize = true;
+            this.lblHeading.Font = new System.Drawing.Font("NSimSun", 16.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHeading.Location = new System.Drawing.Point(131, 32);
+            this.lblHeading.Name = "lblHeading";
+            this.lblHeading.Size = new System.Drawing.Size(312, 28);
+            this.lblHeading.TabIndex = 0;
+            this.lblHeading.Text = "MULTITHREADING IN C#";
+            this.lblHeading.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnRun
+            // 
+            this.btnRun.Location = new System.Drawing.Point(136, 130);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(307, 69);
+            this.btnRun.TabIndex = 1;
+            this.btnRun.Text = "RUN PROCESS";
+            this.btnRun.UseVisualStyleBackColor = true;
+            this.btnRun.Click += new System.EventHandler(this.btnRun_Click);
+            // 
+            // prbProcessProgress
+            // 
+            this.prbProcessProgress.Location = new System.Drawing.Point(136, 219);
+            this.prbProcessProgress.Name = "prbProcessProgress";
+            this.prbProcessProgress.Size = new System.Drawing.Size(307, 69);
+            this.prbProcessProgress.TabIndex = 2;
+            // 
+            // btnShowMessage
+            // 
+            this.btnShowMessage.Location = new System.Drawing.Point(136, 309);
+            this.btnShowMessage.Name = "btnShowMessage";
+            this.btnShowMessage.Size = new System.Drawing.Size(307, 69);
+            this.btnShowMessage.TabIndex = 3;
+            this.btnShowMessage.Text = "SHOW MESSAGE";
+            this.btnShowMessage.UseVisualStyleBackColor = true;
+            this.btnShowMessage.Click += new System.EventHandler(this.btnShowMessage_Click);
+            // 
+            // Multithreading
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(606, 461);
+            this.Controls.Add(this.btnShowMessage);
+            this.Controls.Add(this.prbProcessProgress);
+            this.Controls.Add(this.btnRun);
+            this.Controls.Add(this.lblHeading);
+            this.Name = "Multithreading";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblHeading;
+        private System.Windows.Forms.Button btnRun;
+        private System.Windows.Forms.ProgressBar prbProcessProgress;
+        private System.Windows.Forms.Button btnShowMessage;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace multithreaded_code
+{
+    public partial class Multithreading : Form
+    {
+        public Multithreading()
+        {
+            InitializeComponent();
+        }
+
+        //simulates activities that require a long runtime
+        private void simulatingRunning(int length)         //length parameter gives the length of time to simulate running in milliseconds
+        {
+            Thread.Sleep(length);                          // pauses the execution of the thread it is run in by the number of milliseconds specified by the parameter
+        }
+
+        private void updateProgress(double currentVal, double maxVal)
+        {
+            int percentage = (int)Math.Round((currentVal / maxVal) * 100);
+
+            if (this.IsHandleCreated)
+            {
+                this.Invoke((MethodInvoker)delegate
+                {
+                    prbProcessProgress.Value = percentage;
+                });
+            }
+
+        }
+
+        private void btnRun_Click(object sender, EventArgs e)
+        {
+
+            double curVal = 0.0;
+            double maxVal = 50.0;
+            bool continueMonitoring = true;
+
+            Thread monitoringThread = new Thread(() =>
+            {
+                while (continueMonitoring)
+                {
+                    updateProgress(curVal, maxVal);
+
+                    Thread.Sleep(250); // Update every 250 milliseconds
+                }
+
+                updateProgress(1, 1); // Ensure progress bar is full at the end
+            });
+
+            monitoringThread.Start();
+
+            FileIOSimulator.File[] directory = FileIOSimulator.Directory.getFiles();
+            List<FileIOSimulator.File>[] threadContents = new List<FileIOSimulator.File>[4];
+
+            for (int i = 0; i < threadContents.Length; i++)
+                threadContents[i] = new List<FileIOSimulator.File>();
+
+            int listIndex = 0;
+
+            for (int i = 0; i < directory.Length; i++)
+            {
+                threadContents[listIndex].Add(directory[i]);
+                listIndex++;
+
+                if (listIndex == 4)
+                    listIndex = 0;
+            }
+
+            maxVal = directory.Length;
+
+            for (int i = 0; i < threadContents.Length; i++)
+            {
+                bool leftCritical = false;
+
+                Thread thrd = new Thread(() =>
+                {
+                    // each thread the files list is passed to the thread
+                    List<FileIOSimulator.File> thisThreadsList = threadContents[i];
+
+                    leftCritical = true;
+
+                    foreach (FileIOSimulator.File file in thisThreadsList)
+                    {
+                        file.readFile();
+                        simulatingRunning(1);
+
+                        curVal++;
+                    }
+
+                    if (curVal == maxVal)
+                        continueMonitoring = false;
+
+                });
+
+                thrd.Start();
+
+                while (!leftCritical)
+                    Thread.Sleep(1);
+            }
+
+        }
+
+        private void btnShowMessage_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("This is a simple message");
+            func(15, 100);
+        }
+
+        delegate void calcPercentage(double val, double maxVal);
+
+        calcPercentage func = delegate (double x, double max)
+        {
+            MessageBox.Show("The percentage value is " + (x / max) * 100.0);
+            List<int> numbers = new List<int>() { 1, 5, 6, 85, 84, 55, 9, 4, 77, 2, 6, 7, 1 };
+            int evens = numbers.Count(n => n % 2 == 0);
+        };
+
+        public class FileIOSimulator
+        {
+            public class File
+            {
+                private int fileReadTimeMilliseconds;
+
+                public void readFile()
+                {
+                    Thread.Sleep(fileReadTimeMilliseconds);
+                }
+
+                public File(int fileReadTimeMilliseconds)
+                {
+                    this.fileReadTimeMilliseconds = fileReadTimeMilliseconds;
+                }
+            }
+
+            public class Directory
+            {
+                public static File[] getFiles()
+                {
+                    Random rand = new Random();
+
+                    Thread.Sleep(rand.Next(5, 50));
+
+                    File[] rArr = new File[rand.Next(1, 200)];
+
+                    for (int i = 0; i < rArr.Length; i++)
+                        rArr[i] = new File(rand.Next(1, 80));
+
+                    return rArr;
+                }
+
+            }
+        }
+
+    }
+}
